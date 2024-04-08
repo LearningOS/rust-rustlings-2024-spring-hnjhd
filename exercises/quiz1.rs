b708125463429582bd25af820ec4803118131b3b
hnjhd <1746190646@qq.com>
2024-04-08 21:00:13 +0800

    test2

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,11 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
-
+fn calculate_price_of_apples(cnt:i32) ->i32{
+    if cnt>40{
+      cnt
+    }else{
+    cnt<<1
+    }
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
